24974a0926ddb8daec0074ece325e62bb7df254e
HumberEnjoyer <n01637285@students.humber.ca>
2024-06-13 14:38:28 -0400

    upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let myNumber = 10;
+let myString = "hello, JavaScript";
+
+console.log(myNumber, myString);
+
+for (let i=0; i <= 10; i+=1){
+    console.log(i);
+};
+
+for (let i=1; i <= 5; i++){
+    console.log(3 * i);
+}
+
+function addNumbers(num1, num2){
+    return num1 + num2
+}
+console.log(addNumbers(3, 4))
+
+function multiplyNumbers(num1, num2){
+    return num1 * num2
+}
+console.log(multiplyNumbers(14, 3))
